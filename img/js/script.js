b26d14e3d2ec122cc6ef90410c46a76ea5c9387f
Beudaert Marianne <x_Mariannee@hotmail.fr>
2019-09-19 09:51:22 +0200

    new version CV

---
+++ img/js/script.js
@@ -0,0 +1,17 @@
+function change()
+{
+    document.getElementById("btn__cv").style.borderColor= "white";
+    document.getElementById("header").style.backgroundImage="url(img/boat.jpg)";
+    
+    document.getElementById("img__presentation").style.display="none";
+    let title= document.getElementById("title").style;
+    title.marginTop="0";
+    title.paddingTop="7.2em";
+}
+
+let btn= document.getElementById("btn__cv");
+let btnScd= document.getElementById("btn__profil");
+btn.addEventListener("click", change);
+btnScd.addEventListener("click", change);
+
+
